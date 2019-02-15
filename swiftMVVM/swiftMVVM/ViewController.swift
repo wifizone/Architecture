a2bae0ed8bdd4757895d8f973b9769ebb400742a
Anton Poluianov <poluyanov.a.an@sberbank.ru>
2019-02-15 10:57:24 +0300

    Added swift MVVM empty project

---
+++ swiftMVVM/swiftMVVM/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  swiftMVVM
+//
+//  Created by Anton Poluianov on 15/02/2019.
+//  Copyright © 2019 Anton Poluianov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
